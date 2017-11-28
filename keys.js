abe106799489061a2edeaaeef8d7212beffc92e4
Seth Walton <sdwalton83@gmail.com>
2017-11-28 01:07:23 -0600

    big three work need Random

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'qDwSUU4D9gTtQGyMBC2Tq4cMQ',
+  consumer_secret: 'GbmlMoYkCd6iYbdDHAQmbscaVcPGPzAIj1358pk8YH49nUdN4i',
+  access_token_key: '931317101666734081-xjfiZPFODObqHI2RHmfrilczbMTQOOp',
+  access_token_secret: 'uVlql63iJI4HjS7hpzbEVUhG9laiq2gt19s1ovtelBkfz',
+}
+
+module.exports = twitterKeys;
