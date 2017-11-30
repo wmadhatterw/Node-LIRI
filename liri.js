d6cb83df49aa8a203fa9d1a1c78faea2c5c7815c
Seth Walton <sdwalton83@gmail.com>
2017-11-29 23:45:58 -0600

    everything working but needs cleanup

--- liri.js
+++ liri.js
@@ -16,9 +16,9 @@ function getSong(){
           secret: 'e6be21f7b21d47f88154479b666abcc9'
      });
      if (inTwo === undefined) {
-            inTwo = "superman";
+            inTwo = "The Sign";
        }
-     spotify.search({ type: 'track', query: inTwo}, function(err, data) {
+     spotify.search({ type: 'track', query: inTwo, limit: "1"}, function(err, data) {
           if (err) {
                return console.log('Error occurred: ' + err);
           }
@@ -26,11 +26,14 @@ function getSong(){
           // console.log(data.tracks.items);
 
           data.tracks.items.forEach(function(value){
+               fs.appendFileSync('log.txt', "Spotify THIS: " + "\n" + "Artist Name: " + value.artists[0].name + "\n" +
+                    "Song Name: " + value.name + "\n" + "Preview Link: " + value.preview_url +"\n" + "Album Name: " + value.album.name +
+                    "\n"+ "=============================" +"\n");
                console.log("Artist Name: " + value.artists[0].name);
                console.log("Song Name: " + value.name);
                console.log("Preview Link: " + value.preview_url);
-               console.log("Alumb Name: " + value.album.name);
-               console.log("_______________________");
+               console.log("Album Name: " + value.album.name);
+               console.log("============================");
                console.log("");
 
           })
@@ -52,10 +55,13 @@ var twitterKeys = require("./keys.js");
 
      client.get('statuses/user_timeline', limit, function(error, tweets, response) {
           if (!error) {
+              
                tweets.forEach(function(value){
+                    fs.appendFileSync('log.txt', "Tweet: " + value.text + "\n" + "Date: " +
+                         value.created_at + "\n" + "================================" + "\n");
                     console.log("Tweet: " + value.text);
                     console.log("Date: " + value.created_at);
-                    console.log("---------------------------------------------------");
+                    console.log("===============================================");
                     console.log("");
                })
           }
@@ -76,50 +82,48 @@ function getMovie() {
 	               
 	 var APIrequest = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=full&apikey=40e9cece";
 
-	               request(APIrequest, function(error, response, body){
-	                    if (!error && response.statusCode === 200) {
+          request(APIrequest, function(error, response, body){
+               if (!error && response.statusCode === 200) {
+                    fs.appendFileSync('log.txt', "Movie Title: " + JSON.parse(body).Title +
+                         "\n" + "Release Year: " + JSON.parse(body).Year + "\n" +
+                         "IMDB Rating: " + JSON.parse(body).Ratings[0].Value + "\n" +
+                         "Rotten Tomaties Rating: " + JSON.parse(body).Ratings[1].Value + "\n" +
+                         "Country where movie was produced: " + JSON.parse(body).Country + "\n" +
+                         "Movie language: " + JSON.parse(body).Language + "\n" + 
+                         "Actors: " + JSON.parse(body).Actors + "\n" +
+                         "=======================================================");
 
-	                        // Parse the body of the site and recover just the imdbRating
-	                        console.log("Movie Title: " + JSON.parse(body).Title);
-	                        console.log("Release Year: " + JSON.parse(body).Year);
-	                        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-	                        console.log("Rotten Tomaties Rating: " + JSON.parse(body).Ratings[1].Value);
-	                        console.log("Country where movie was produced: " + JSON.parse(body).Country);
-	                        console.log("Movie language: " + JSON.parse(body).Language);
-	                        console.log("Movie plot: " + JSON.parse(body).Plot);
-	                        console.log("Actors: " + JSON.parse(body).Actors);
-	                        console.log("");
-	                      }
+                        // Parse the body of the site and recover just the imdbRating
+                        console.log("Movie Title: " + JSON.parse(body).Title);
+                        console.log("Release Year: " + JSON.parse(body).Year);
+                        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+                        console.log("Rotten Tomaties Rating: " + JSON.parse(body).Ratings[1].Value);
+                        console.log("Country where movie was produced: " + JSON.parse(body).Country);
+                        console.log("Movie language: " + JSON.parse(body).Language);
+                        console.log("Movie plot: " + JSON.parse(body).Plot);
+                        console.log("Actors: " + JSON.parse(body).Actors);
+                        console.log("");
+                 }
 
-	               })
-	          }
+          })
+     }
 
-// // Function Random File
+// Function Random File
 
-// function getRandom(){
-// 	fs.readFile("random.txt", "utf8", function(error, data){
-// 		if(error){
-// 			console.log(error);
-// 		} else{
-// 			var dataArray = data.split(',');
-// 			var inOne = dataArray[0];
-// 			var intwo = dataArray[1];
-// 			switch(inOne) {
-// 				case "my-tweets":
-// 					getTweets();
-// 					break;
-// 				case "spotify-this-song":
-// 					getSong();
-// 					break;
-// 				case "movie-this":
-// 					getMovie();
-// 					break;
-// 			}
-// 		}
-// 	});
-// };
-
-switch(inOne) {
+function getRandom(){
+	fs.readFile("random.txt", "utf8", function(error, data){
+		if(error){
+			console.log(error);
+		} else{
+			var dataArray = data.split(',');
+               console.log("========================")
+               console.log("========================")
+               console.log(dataArray)
+			inOne = dataArray[0];
+               console.log(dataArray[0])
+			inTwo = dataArray[1];
+               console.log(dataArray[1])
+			switch(inOne) {
 				case "my-tweets":
 					getTweets();
 					break;
@@ -129,9 +133,30 @@ switch(inOne) {
 				case "movie-this":
 					getMovie();
 					break;
-				default:
-          console.log("Please select from 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says'");
+			}
+		}
+	});
 };
+
+
+switch(inOne) {
+		case "my-tweets":
+			getTweets();
+			break;
+		case "spotify-this-song":
+			getSong();
+			break;
+		case "movie-this":
+			getMovie();
+			break;
+          case "do-what-it-says":
+               getRandom();
+          break;
+          default:
+               console.log("Please select from 'my-tweets', 'spotify-this-song', 'movie-this'");
+               console.log(" or 'do-what-it-says'")
+}
+				
 
 
 
